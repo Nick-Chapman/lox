793a9039970dd4021d14163fafb84ea55ea200cf
Nick Chapman <nic@amadido.co.uk>
2025-12-31 10:13:49 +0000

    chap08; operator; all 29 tests pass

--- src/Lox.hs
+++ src/Lox.hs
@@ -6,11 +6,13 @@ import System.Environment(getArgs)
 import System.FilePath (takeFileName)
 import Text.Printf (printf)
 import qualified Data.Char as Char
+import System.Exit(ExitCode(..),exitWith)
+import System.IO (stderr,hFlush,hPutStrLn)
 
 main :: IO ()
 main = do
   getArgs >>= \case
-    [] -> undefined --repl
+    [] -> error "repl"
     _:_:_ -> error "too any args"
     [filename] -> do
       contents <- readFile filename
@@ -48,7 +50,7 @@ data Lit
 
 data Op1 = Negate
 
-data Op2 = Add | Sub | Equal
+data Op2 = Add | Sub | Mul | Div | Equals | NotEquals | Less | LessEqual | Greater | GreaterEqual
 
 ----------------------------------------------------------------------
 -- Execution
@@ -63,17 +65,26 @@ execD = \case
 
 execS :: Stat -> IO ()
 execS = \case
-  SExp e -> do _ <- pure $ eval e; pure ()
-  SPrint e -> print (eval e)
+  SExp e -> do
+    _ <- eval e
+    pure ()
+  SPrint e -> do
+    v <- eval e
+    print v
 
 ----------------------------------------------------------------------
 -- Evaluation
 
-eval :: Exp -> Value
+eval :: Exp -> IO Value
 eval = \case
-  ELit x -> evalLit x
-  EBinary e1 op e2 -> evalOp2 op (eval e1) (eval e2)
-  EUnary op e -> evalOp1 op (eval e)
+  ELit x -> pure (evalLit x)
+  EBinary e1 op e2 -> do
+    v1 <- eval e1
+    v2 <- eval e2
+    evalOp2 v1 v2 op
+  EUnary op e -> do
+    v <- eval e
+    evalOp1 op v
   where
     evalLit = \case
       LNil{} -> VNil
@@ -84,15 +95,38 @@ eval = \case
     evalOp1 = \case
       Negate -> vnegate
 
-    evalOp2 = \case
-      Add -> vadd
-      Sub -> vsub
-      Equal -> \v1 v2 -> VBool (vequal v1 v2)
+    evalOp2 v1 v2 = \case
+      Add -> vadd (v1,v2)
+      Sub -> VNumber <$> binary (-) v1 v2
+      Mul -> VNumber <$> binary (*) v1 v2
+      Div -> VNumber <$> binary (/) v1 v2
+      Equals -> pure (VBool (vequal v1 v2))
+      NotEquals -> pure (VBool (not (vequal v1 v2)))
+      Less -> VBool <$> binary (<) v1 v2
+      LessEqual -> VBool <$> binary (<=) v1 v2
+      Greater -> VBool <$> binary (>) v1 v2
+      GreaterEqual -> VBool <$> binary (>=) v1 v2
 
-    vadd v1 v2 = VNumber (getNumber v1 + getNumber v2)
-    vsub v1 v2 = VNumber (getNumber v1 - getNumber v2)
+    binary f v1 v2 = do
+      n1 <- getNumber "Operands must be numbers." v1
+      n2 <- getNumber "Operands must be numbers." v2
+      pure (f n1 n2)
 
-    vnegate v1 = VNumber (negate $ getNumber v1)
+vnegate :: Value -> IO Value
+vnegate v1 = do
+  n1 <- getNumber "Operand must be a number." v1
+  pure (VNumber (negate n1))
+
+getNumber :: String -> Value -> IO Double
+getNumber message = \case
+  VNumber n -> pure n
+  _ -> abort message
+
+vadd :: (Value,Value) -> IO Value
+vadd = \case
+  (VNumber n1, VNumber n2) -> pure (VNumber (n1 + n2))
+  (VString s1, VString s2) -> pure (VString (s1 ++ s2))
+  _ -> abort "Operands must be two numbers or two strings."
 
 vequal :: Value -> Value -> Bool
 vequal v1 v2 = case (v1,v2) of
@@ -101,6 +135,18 @@ vequal v1 v2 = case (v1,v2) of
   (VNumber n1, VNumber n2) -> n1 == n2
   (VString s1, VString s2) -> s1 == s2
   _ -> False
+
+abort :: String -> IO a
+abort mes = do
+  putErr mes
+  putErr "[line 1]" -- hack -- TODO: pass line number from caller
+  exitWith (ExitFailure 70)
+
+putErr :: String -> IO ()
+putErr s = do
+  hPutStrLn stderr s
+  hFlush stderr
+  pure ()
 
 ----------------------------------------------------------------------
 -- Values
@@ -111,11 +157,6 @@ data Value
   | VNumber Double
   | VString String
 
-getNumber :: Value -> Double
-getNumber = \case
-  VNumber n -> n
-  v -> error (show ("getNumber",v))
-
 instance Show Value where
   show = \case
     VNil -> "nil"
@@ -123,7 +164,7 @@ instance Show Value where
     VNumber n -> do
       let s = printf "%f" n
       if ".0" `isSuffixOf` s then reverse $ drop 2 $ reverse s else s
-    VString s -> show s
+    VString s -> s
 
 ----------------------------------------------------------------------
 -- Parse
@@ -156,14 +197,26 @@ gram = program where
 
   equality = do
     e1 <- comparison
-    alts [ do op <- alts [ do key "=="; pure Equal
+    alts [ do op <- alts [ do key "=="; pure Equals
+                         , do key "!="; pure NotEquals
                          ]
               e2 <- comparison
               pure (EBinary e1 op e2)
          , pure e1
          ]
 
-  comparison = term
+  comparison = do
+    e1 <- term
+    alts [ do op <- alts [ alts []
+                         , do key "<="; pure LessEqual
+                         , do key "<"; pure Less
+                         , do key ">="; pure GreaterEqual
+                         , do key ">"; pure Greater
+                         ]
+              e2 <- term
+              pure (EBinary e1 op e2)
+         , pure e1
+         ]
 
   term = do
     e1 <- factor
@@ -175,14 +228,24 @@ gram = program where
          , pure e1
          ]
 
-  factor = unary
+  factor = do
+    e1 <- unary
+    alts [ do op <- alts [ do key "*"; pure Mul
+                         , do key "/"; pure Div
+                         ]
+              e2 <- unary
+              pure (EBinary e1 op e2)
+         , pure e1
+         ]
 
   unary = alts
     [ do op <- alts [ do key "-"; pure Negate ]
-         e <- primary
+         e <- unary
          pure (EUnary op e)
-    , primary
+    , call
     ]
+
+  call = primary
 
   primary :: Par Exp
   primary = alts
@@ -206,16 +269,25 @@ gram = program where
     pure x
 
   numberLit = nibble (read <$> numberChars)
+    where
+      numberChars = do
+        before <- some digit
+        alts
+          [ do lit '.'
+               after <- some digit
+               pure (before ++ "." ++ after)
+          , pure before
+          ]
+      digit = sat Char.isDigit
 
   stringLit = nibble $ do
     doubleQuote
     x <- many stringLitChar
     doubleQuote
     pure x
-
-  doubleQuote = lit '"'
-
-  stringLitChar = sat $ \c -> c /= '"'
+    where
+      doubleQuote = lit '"'
+      stringLitChar = sat $ \c -> c /= '"'
 
   key chars = nibble (noError (mapM_ lit chars))
 
@@ -224,23 +296,13 @@ gram = program where
     whitespace
     pure x
 
-  numberChars = do
-    before <- some digit
-    alts
-      [ do lit '.'
-           after <- some digit
-           pure (before ++ "." ++ after)
-      , pure before
-      ]
-
-  digit = sat Char.isDigit
-
   whitespace = skip (alts [white1, commentToEol])
 
   white1 = alts [lit ' ', lit '\n', lit '\t']
 
+  commentToEol :: Par ()
   commentToEol = do
-    lit '/'; lit '/'
+    noError (do lit '/'; lit '/')
     skip notNL
     lit '\n'
 
