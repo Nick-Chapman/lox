268e98b2eb59a31e57c9a5f5dfb6d5873c97f021
Nick Chapman <nic@amadido.co.uk>
2026-01-15 09:23:47 +0000

    fix testing rules to be deterministic about order of stdout & stderr

--- rules.sh
+++ rules.sh
@@ -10,10 +10,10 @@ cat <<EOF
 $x.lox : $abs : ln $abs $x.lox
 
 $x.expect : $x.lox
-  ~/other/craftinginterpreters/clox $x.lox 2>&1 | cat > $x.expect  || true
+  ~/other/craftinginterpreters/clox $x.lox 2>err >$x.expect; cat err >>$x.expect
 
 $x.actual : $x.lox src/lox.exe
-  ./lox.exe $x.lox 2>&1 | cat > $x.actual || true
+  ./lox.exe $x.lox 2>err >$x.actual; cat err >>$x.actual
 
 *test-$x: $x.expect $x.actual
   git diff --color $x.expect $x.actual
