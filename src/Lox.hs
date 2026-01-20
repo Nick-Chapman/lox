4f488f09527e2aa731a13adaac7af3e5aeef2a5b
Nick Chapman <nic@amadido.co.uk>
2026-01-20 13:39:22 +0000

    take flag on lox command line to select interpretation mode

--- src/Lox.hs
+++ src/Lox.hs
@@ -1,11 +1,12 @@
 module Lox (main) where
 
+import Ast (Stat)
+import Compiler qualified (executeTopDecls)
 import Data.Text qualified as Text
 import Interpreter qualified (executeTopDecls)
-import Compiler qualified (executeTopDecls)
 import Parser qualified (tryParse)
 import Resolver qualified (resolveTop)
-import Runtime (runEffect)
+import Runtime (Eff,runEffect)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(..),exitWith)
 import System.IO (stdout,stderr,hFlush,hPutStrLn,hSetBinaryMode)
@@ -13,7 +14,8 @@ import System.IO.Binary (readBinaryFile)
 
 main :: IO ()
 main = do
-  getArgs >>= \case
+  Config{files,mode} <- parseArgs <$> getArgs
+  case files of
     [] -> error "no repl"
     _:_:_ -> error "too any args"
     [filename] -> do
@@ -24,15 +26,36 @@ main = do
           case Resolver.resolveTop decls of
             errs@(_:_)-> abort 65 errs
             [] ->
-              Runtime.runEffect putOut (_interpretBC decls) >>= \case
+              Runtime.runEffect putOut (interpret mode decls) >>= \case
                 Right _globals' -> do
                   -- if/when support repl, we have the updated globals in hand
                   pure ()
                 Left err -> do
                   abort 70 [err]
         where
-          _interpret = Interpreter.executeTopDecls
-          _interpretBC = Compiler.executeTopDecls
+
+
+interpret :: Mode -> [Stat] -> Eff ()
+interpret mode stats =
+  case mode of
+    ModeTree -> do _ <- Interpreter.executeTopDecls stats; pure ()
+    ModeBCI -> Compiler.executeTopDecls stats
+
+data Mode = ModeTree | ModeBCI
+
+data Config = Config { files :: [String], mode :: Mode }
+
+parseArgs :: [String] -> Config
+parseArgs = loop Config { files = [], mode = defaultMode }
+  where
+    defaultMode = ModeTree
+    loop acc = \case
+      [] -> acc
+      "-tree":xs -> loop acc { mode = ModeTree } xs
+      "-bci":xs -> loop acc { mode = ModeBCI } xs
+      flag@('-':_):_ -> error ("unknown flag: " ++ flag)
+      file:xs -> loop acc { files = file : files acc } xs
+
 
 abort :: Int -> [String] -> IO a
 abort code errs = do
--- test/rules.sh
+++ test/rules.sh
@@ -13,7 +13,7 @@ $x.expect : reference-lox $x.lox looseSecondaryErrors.sh
   ./reference-lox $x.lox 2>err >$x.expect; cat err | ./looseSecondaryErrors.sh >>$x.expect
 
 $x.actual : exe-under-test $x.lox looseSecondaryErrors.sh
-  ./exe-under-test $x.lox 2>err >$x.actual; cat err | ./looseSecondaryErrors.sh >>$x.actual
+  ./exe-under-test $(cat args-for-exe-under-test) $x.lox 2>err >$x.actual; cat err | ./looseSecondaryErrors.sh >>$x.actual
 
 *test-$x: $x.expect $x.actual
   git diff --color $x.expect $x.actual
