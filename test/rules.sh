c2c230f900dcfbbb515f33e80e39c3d5e053a2ad
Nick Chapman <nic@amadido.co.uk>
2026-01-28 08:51:53 +0000

    run tests for my lox in 3 modes: T,H,C

--- test/rules.sh
+++ test/rules.sh
@@ -9,14 +9,28 @@ cat <<EOF
 
 $x.lox : $abs : ln $abs $x.lox
 
+# Reference...
+
 $x.expect : reference-lox $x.lox looseSecondaryErrors.sh
   ./reference-lox $x.lox 2>err >$x.expect; cat err | ./looseSecondaryErrors.sh >>$x.expect
 
-$x.actual : exe-under-test @deps-for-exe-under-test $x.lox looseSecondaryErrors.sh
-  ./exe-under-test $x.lox 2>err >$x.actual; cat err | ./looseSecondaryErrors.sh >>$x.actual
+$x.T : T.sh @deps-for-T $x.lox looseSecondaryErrors.sh
+  ./T.sh $x.lox 2>err >$x.T; cat err | ./looseSecondaryErrors.sh >>$x.T
 
-*test-$x: $x.expect $x.actual
-  git diff --text --color $x.expect $x.actual
+*test-T-$x: $x.expect $x.T
+  git diff --text --color $x.expect $x.T
+
+$x.H : H.sh @deps-for-H $x.lox looseSecondaryErrors.sh
+  ./H.sh $x.lox 2>err >$x.H; cat err | ./looseSecondaryErrors.sh >>$x.H
+
+*test-H-$x: $x.expect $x.H
+  git diff --text --color $x.expect $x.H
+
+$x.C : C.sh @deps-for-C $x.lox looseSecondaryErrors.sh
+  ./C.sh $x.lox 2>err >$x.C; cat err | ./looseSecondaryErrors.sh >>$x.C
+
+*test-C-$x: $x.expect $x.C
+  git diff --text --color $x.expect $x.C
 
 EOF
 
