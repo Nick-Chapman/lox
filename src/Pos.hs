19cd887c5ed973cd1ee8d46c5a0fd9c78c632cc3
Nick Chapman <nic@amadido.co.uk>
2026-02-03 15:53:36 +0000

    remove module time selection of Pos = JustLine/LineCol, now we have runtime selection (-col)

--- src/Pos.hs
+++ src/Pos.hs
@@ -1,3 +1,27 @@
 
-module Pos (module X) where
-import LineCol as X -- select LineCol or JustLine here
+module Pos (Pos,toLine,ofLine,initPos,tickPos,Mode(..),showPos) where
+
+import Text.Printf (printf)
+
+data Pos = Pos { line :: Int, col :: Int }
+
+toLine :: Pos -> Int
+toLine Pos {line} = line
+
+ofLine :: Int -> Pos
+ofLine line = Pos { line, col = 0 }
+
+initPos :: Pos
+initPos = Pos { line = 1, col = 0 }
+
+tickPos :: Pos -> Char -> Pos
+tickPos Pos {line,col} = \case
+  '\n' -> Pos { line = 1 + line, col = 0 }
+  _ -> Pos { line, col = col + 1 }
+
+data Mode = Brief | Verbose
+
+showPos :: Mode -> Pos -> String
+showPos = \case
+  Brief -> \Pos{line} -> printf "[line %d]" line
+  Verbose -> \Pos{line,col} -> printf "[line %d, column %d]" line col
